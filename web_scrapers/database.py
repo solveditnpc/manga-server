a2afcd9b480a424d6937ad8429b0a34762422dc9
solveditnpc <neutralwritergithubdedicated@gmail.com>
2025-12-25 00:13:12 +0530

    to avoid crash

--- web_scrapers/database.py
+++ web_scrapers/database.py
@@ -9,7 +9,7 @@ DB_PATH = "/database/manga_database.db"
 def get_db_connection():
     conn = sqlite3.connect(DB_PATH)
     conn.row_factory = sqlite3.Row
-    conn.execute('PRAGMA journal_mode=WAL;')
+    #conn.execute('PRAGMA journal_mode=WAL;')
     conn.execute('PRAGMA busy_timeout = 5000;')
     return conn
 
