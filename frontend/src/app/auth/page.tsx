ee4e042661b7d1bcb3a32be33ee4b5c8a71d6f6d
Manmohit Singh <manmohit249@gmail.com>
2025-12-26 01:43:46 +0530

    feat(auth): add single-page username-based auth UI
    implement username + password form
    
    same form will be used for registering as well as logging in
    
    currently frontend only ,so simulating login state using client side cookies
    
    add temporary home page with cookie-based auth check and logout simulation

---
+++ frontend/src/app/auth/page.tsx
@@ -0,0 +1,38 @@
+import AuthForm from "@/components/AuthForm";
+import Image from "next/image";
+export default function AuthPage() {
+	return (
+		<div className="h-screen w-screen">
+			<div className="h-screen w-screen absolute">
+				<Image src="/bg.jpg" alt="logo" fill className="object-cover" />
+				<div className="absolute inset-0 bg-black/87 backdrop-blur-xs"></div>{" "}
+			</div>
+			<div className="min-h-screen flex items-center justify-center">
+				<div className="w-full max-w-sm px-6 z-10">
+					<div className="bg-card opacity-90 border border-default rounded-lg p-6">
+						<div className="space-y-6">
+							{/* Header */}
+							<header className="space-y-1">
+								<h1 className="text-xl font-medium fg-primary text-center">
+									Continue reading
+								</h1>
+								<p className="text-sm fg-muted text-center">
+									Use a username to keep track of what you read and like.
+								</p>
+							</header>
+							{/* Children ( FORM ) */}
+							<AuthForm />
+
+							{/* Footer note */}
+							<p className="text-xs fg-muted text-center italic">
+								If the username exists, we’ll verify the password.
+								<br />
+								If it’s new, we’ll register it and continue.
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
