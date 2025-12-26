95a92439b633052d8266ec4de455a9b4592057c4
Manmohit Singh <manmohit249@gmail.com>
2025-12-27 01:01:26 +0530

    refactor(auth-ui):refactored files structure

--- frontend/src/components/ui/Tooltip.tsx
+++ frontend/src/components/ui/Tooltip.tsx
@@ -1,25 +1,25 @@
 import { ReactNode } from "react";
 
 export default function Tooltip({ message }: { message: ReactNode }) {
-	return (
-		<span className="relative inline-block group">
-			{/* Trigger */}
-			<span
-				tabIndex={0}
-				className="
+  return (
+    <span className="relative inline-block group">
+      {/* Trigger */}
+      <span
+        tabIndex={0}
+        className="
           ml-1 inline-flex h-4 w-4 items-center justify-center
           rounded-full border border-lighter
           text-[10px] fg-secondary
           cursor-help
           focus-ring
         "
-			>
-				?
-			</span>
+      >
+        ?
+      </span>
 
-			{/* Tooltip */}
-			<span
-				className="
+      {/* Tooltip */}
+      <span
+        className="
           pointer-events-none absolute left-1/2 top-full z-10
           mt-2 w-56 -translate-x-1/2
           rounded-md bg-card border border-default
@@ -27,9 +27,9 @@ export default function Tooltip({ message }: { message: ReactNode }) {
           opacity-0 group-hover:opacity-100 group-focus-within:opacity-100
           transition-opacity
         "
-			>
-				{message}
-			</span>
-		</span>
-	);
+      >
+        {message}
+      </span>
+    </span>
+  );
 }
