c7d5049d3bcbdff06498dcd1cfa065f1c1d01263
solveditnpc <neutralwritergithubdedicated@gmail.com>
2026-01-04 23:07:44 +0530

    installation

--- web_scrapers/start.sh
+++ web_scrapers/start.sh
@@ -2,5 +2,7 @@
 echo "--- Starting Gunicorn API Server (in background) ---"
 gunicorn --workers 1 --bind 0.0.0.0:5002 --access-logfile - --error-logfile - api:app &
 sleep 5
+echo "--- Starting Suwayomi Sync Script (in background) ---"
+python -u suwayomi_sync.py &
 echo "--- Starting Queue Processor (in foreground) ---"
 python -u processor.py
